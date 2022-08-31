14c5b65803ea28550a226c4aae468a0c291491d3
Seren AKYÜZ <serenwq@gmail.com>
2022-08-31 23:57:43 +0300

    Day 3

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommandValidator : AbstractValidator<CreateBrandCommand>
+    {
+        public CreateBrandCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(2);
+        }
+    }
+}
